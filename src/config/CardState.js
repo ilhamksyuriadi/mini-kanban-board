a5661d6ace83a9b2aec1a12064b6e27fab50fc85
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-07-27 21:02:36 +0700

    minor fix

--- src/config/CardState.js
+++ src/config/CardState.js
@@ -2,18 +2,26 @@ export const CardState =     {
   tasks: {
       'dummy-1': { 
           id: 'dummy-1', 
-          title: 'Makan',
+          title: 'Research feature',
           assignee: 'Iyuuu',
           start_date: '27-9-20',
           end_date: '28-9-20',
           tags: 'Research'
+      },
+      'dummy-2': { 
+        id: 'dummy-2', 
+        title: 'Set up server',
+        assignee: 'Iyuuu',
+        start_date: '27-9-20',
+        end_date: '28-9-20',
+        tags: 'Dev Ops'
       }
   },
   cards: {
       'card-1': {
           id: 'card-1',
           title: 'Backlog',
-          taskIds: ['dummy-1'],
+          taskIds: ['dummy-1', 'dummy-2'],
           color: '#FFBA08',
       },
       'card-2': {
